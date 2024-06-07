596c6c769073ce05c00733ea661d5d5974a9b5b4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-06-07 15:24:20 +0100

    create an app that calcul the salary depends on the poste of the employe

---
+++ OOP/Cadre.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'Employe.php';
+
+class Cadre extends Employe {
+    private $salaireBase;
+    private $bonus;
+
+    public function __construct($nom, $id, $poste, $salaireBase, $bonus) {
+        parent::__construct($nom, $id, $poste);
+        $this->salaireBase = $salaireBase;
+        $this->bonus = $bonus;
+    }
+
+    public function calculerSalaire() {
+        $this->salaire = $this->salaireBase + $this->bonus;
+    }
+}
+
+?>
