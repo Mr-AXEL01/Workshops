4d366a0174089ff62eb4631a8e9d4944b88ac6fc
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-06-19 18:41:20 +0100

    set the second exercice where i have a function that check my variable and check if it inférieur or supérieur than 10

---
+++ JS/Exercice2/script.js
@@ -0,0 +1,11 @@
+function checkVariable() {
+    let myVar = 100;
+
+    if (myVar < 10) {
+        console.log('inférieur à 10');
+    } else {
+        console.log('supérieur à 10');
+    }
+}
+
+checkVariable();
